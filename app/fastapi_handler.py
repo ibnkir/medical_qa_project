2b1dbd0225dc835f6295aded025f86e67daf7348
Kirill N <94640267+ibnkir@users.noreply.github.com>
2024-05-17 08:01:45 +0300

    Update fastapi_handler.py

--- app/fastapi_handler.py
+++ app/fastapi_handler.py
@@ -1,10 +1,13 @@
-"""Класс FastApiHandler, который обрабатывает запросы API.
+"""Класс FastApiHandler для обработки запросов API."""
 
-Для тестирования можно ввести команду из терминала: python -m app.fastapi_handler
-"""
-
+import os
+import numpy as np
+import pandas as pd
+from datasets import load_dataset
+from sklearn.metrics import pairwise_distances
 from catboost import CatBoostClassifier
 from sentence_transformers import SentenceTransformer
+import time
 
 
 class FastApiHandler:
@@ -20,7 +23,8 @@ class FastApiHandler:
         # Типы параметров запроса для проверки
         self.param_types = {
             "user_text": str,
-            "questions_num": int
+            "questions_num": int,
+            "is_cls_needed": bool
         }
         
         self.prepare_data()
@@ -29,7 +33,8 @@ class FastApiHandler:
 
     def prepare_data(self):
         """Подготовка данных."""
-        
+        os.makedirs("./data", exist_ok=True)
+
         # Скачиваем датасет с парами вопросов
         try:
             # Скачиваем из локального файла предобработанный датасет, если ранее сохраняли
@@ -40,7 +45,6 @@ class FastApiHandler:
             data = load_dataset("medical_questions_pairs", split="train")
             self.data = pd.DataFrame(data)
             is_preproc_needed = True
-
         
         # Создаем корпус вопросов
         try:
@@ -48,14 +52,12 @@ class FastApiHandler:
             self.questions = pd.read_csv("../data/medical_questions.csv")
         except:
             # Создаем на основе исходного датасета
-            self.questions = pd.DataFrame(pd.concat([data['question_1'], data['question_2']], axis=0).unique())
+            self.questions = pd.DataFrame(pd.concat([self.data['question_1'], self.data['question_2']], axis=0).unique())
             self.questions.reset_index(inplace=True)
             self.questions.rename(columns={'index': 'question_id', 0: 'question'}, inplace=True)    
             # Сохраняем локально
-            os.makedirs("../data", exist_ok=True)
             self.questions.to_csv("../data/medical_questions.csv", index=False)
 
-        
         # Если пары вопросов загружены не локально, а из Интернета, то добавляем id вопросов и сохраняем локально
         if is_preproc_needed:
             # Добавляем id к вопросам из 1-й колонки
@@ -69,24 +71,28 @@ class FastApiHandler:
             self.data.drop(columns='question', inplace=True)
 
             # Сохраняем локально
-            os.makedirs("../data", exist_ok=True)
             self.data.to_csv("../data/medical_questions_pairs.csv", index=False)
         
     def encode_all_questions(self):
         """Векторизация корпуса вопросов с помощью BERT-модели."""
         self.bert_model = SentenceTransformer(self.bert_model_name)
-        self.bert_embeds = self.bert_model.encode(self.questions['question'], convert_to_tensor=False)
+        try:
+            # Скачиваем из локального файла, если ранее сохраняли
+            self.bert_embeds = np.load("./data/question_bert_embeds.npy")
+        except:
+            self.bert_embeds = self.bert_model.encode(self.questions['question'], convert_to_tensor=False)
+            with open('../data/question_bert_embeds.npy', 'wb') as f:
+                np.save(f, self.bert_embeds)
             
     def load_question_pairs_classifier(self):
         """Загрузка обученного классификатора пар вопросов."""
         try:
             self.cb_cls_model = CatBoostClassifier()
-            self.cb_cls_model.load_model('../models/question_pairs_cb_classifier.bin')
-        
+            self.cb_cls_model.load_model('../models/question_pairs_cb_classifier.cbm')
         except Exception as e:
             print(f"Failed to load catboost question pairs classifier: {e}")
 
-    def find_close_embed_inds(self, query_embed, n=10):
+    def find_close_embed_inds(self, query_embed, n, is_cls_needed):
         """
         Поиск индексов n ближайших векторов.
         
@@ -105,14 +111,18 @@ class FastApiHandler:
             close_embed_inds = np.argsort(dists)[1:]
         else:
             close_embed_inds = np.argsort(dists)
-    
+
+        n = min(n, len(close_embed_inds))
+        
+        if not is_cls_needed:
+            return close_embed_inds[:n]
+            
         res_inds = []
         inds_labeled_0 = []
         cnt = 0
-        n = min(n, len(close_embed_inds))
-        
+                
         for idx in close_embed_inds:
-            close_embed = all_embeds[idx].reshape(1, -1)
+            close_embed = self.bert_embeds[idx].reshape(1, -1)
             
             left_pair = np.hstack([query_embed, close_embed])
             if self.cls_model.predict(left_pair)[0] == 1:
@@ -145,7 +155,7 @@ class FastApiHandler:
         Returns:
             bool: True — если есть нужные параметры, False — иначе
         """
-        if "user_text" not in query_params or "questions_num" not in query_params:
+        if "user_text" not in query_params or "questions_num" not in query_params or "is_cls_needed" not in query_params:
             return False
 
         if not isinstance(query_params["user_text"], self.param_types["user_text"]):
@@ -153,10 +163,12 @@ class FastApiHandler:
 
         if not isinstance(query_params["questions_num"], self.param_types["questions_num"]):
             return False
+
+        if not isinstance(query_params["is_cls_needed"], self.param_types["is_cls_needed"]):
+            return False
         
         return True
-        
-              
+          
     def handle(self, query_params):
         """Функция для обработки входящих запросов по API. 
         Запрос состоит из текста вопроса и количества похожих на него вопросов, которое нужно найти.
@@ -168,22 +180,26 @@ class FastApiHandler:
             - **dict**: Словарь, содержащий результат выполнения запроса.
         """
         try:
-            # валидируем запрос к API
+            # Проверяем запрос к API
             if not self.check_query_params(query_params):
                 response = {"Error": "Problem with query parameters"}
             else:
+                        
                 user_text = query_params["user_text"]
                 questions_num = query_params["questions_num"]
+                is_cls_needed = query_params["is_cls_needed"]
                 
-                query_embed = self.bert_model.encode(query_text, convert_to_tensor=False).reshape(1, -1)
-                inds = self.find_close_embed_inds(query_embed, questions_num)
+                start_time = time.time()
+                query_embed = self.bert_model.encode(user_text, convert_to_tensor=False).reshape(1, -1)
+                inds = self.find_close_embed_inds(query_embed, questions_num, is_cls_needed)
                 similar_questions = self.questions.iloc[inds, :]['question'].values
                 
                 response = {
-                    "similar_questions": similar_questions
+                    "similar_questions": list(similar_questions),
+                    'time': time.time() - start_time
                 }
+
         except Exception as e:
-            print(f"Error while handling request: {e}")
             return {"Error": "Problem with request"}
         else:
             return response
@@ -194,13 +210,14 @@ if __name__ == "__main__":
     # Создаём параметры для тестового запроса
     test_params = {
         "user_text": "How do I check my blood sugar?",
-        "questions_num": 10
+        "questions_num": 10,
+        "is_cls_needed": False
     }
 
-    # создаём обработчик запросов для API
+    # Создаём обработчик запросов для API
     handler = FastApiHandler()
 
     # Делаем тестовый запрос
-    print(f"Searching {test_params['questions_num']} similar questions for the query:\n{test_params['user_text']}")
+    print(f"Searching {test_params['questions_num']} similar questions for text:\n{test_params['user_text']}\n")
     response = handler.handle(test_params)
     print(f"Response: {response}")
